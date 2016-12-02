42f7e6799caf4963b525759e1f1c176d23d30aa4
can <helloyoucan@163.com>
2016-12-02 21:14:32 +0800

    订餐html模板
    这次完善了订餐页面

---
+++ src/OrderSys/src/main/webapp/订餐html/js/textscrolling.js
@@ -0,0 +1,19 @@
+var $area = $('.cn-msg'),
+	$con1 = $('.con1'),
+	$con2 = $('.con2'),
+	speed = 5000;
+$con2.append($con1.html()); //克隆内容
+function textscrollUp() {
+	if($area.scrollTop() >= $con1.height() - $area.height() + 30) {
+		$area.scrollTop(0);
+	}
+	$area.animate({
+		'scrollTop': '+=30'
+	}, 1000);
+}
+var myScroll = setInterval("textscrollUp()", speed);
+$area.on('mouseover', function() {
+	clearInterval(myScroll);
+}).on('mouseout', function() {
+	myScroll = setInterval("textscrollUp()", speed);
+});
